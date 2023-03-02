0c2e3cf3580cb8de4d3566bf926c5cd39915a5c9
romanconstantin1 <romanconstantin1@gmail.com>
2023-03-02 13:35:17 +0000

    first draft of data model

--- src/models.py
+++ src/models.py
@@ -8,26 +8,66 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name = Column(String (250), nullable=False)
+    last_name = Column(String (250))
+    user_name = Column(String (250), nullable=False)
+    password = Column(String (250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    def __repr__(self):
+        return '<User %r>' % self.user_name
+
+class Entry(Base):
+    __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    entry_name = Column(String (250), ForeignKey('users.id'))
+    fetch_link = Column(String (250), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Character(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    char_name = Column(String (250), ForeignKey('entries.fetch_link'))
+    char_height = Column(Integer, nullable=False)
+    char_mass = Column(Integer, nullable=False)
+    char_hair_color = Column(String (250))
+    char_skin_color = Column(String (250), nullable=False)
+    char_eye_color = Column(String (250), nullable=False)
+    char_birth_year = Column(String (250), nullable=False)
+    char_gender = Column(String (250))
+
+class Planet(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String (250), ForeignKey('entries.fetch_link'))
+    planet_diameter = Column(Integer, nullable=False)
+    planet_gravity = Column(String(250), nullable=False)
+    planet_population = Column(String(250), nullable=False)
+    planet_climate = Column(String(250), nullable=False)
+    planet_terrain = Column(String(250), nullable=False)
+
+class Ship(Base):
+    __tablename__ = 'ships'
+    id = Column(Integer, primary_key=True)
+    ship_name = Column(String (250), ForeignKey('entries.fetch_link'))
+    ship_manufacturer = Column(String (250), nullable=False)
+    ship_length = Column(Integer, nullable=False)
+    ship_crew = Column(String(250), nullable=False)
+    ship_passenger = Column(Integer, nullable=False)
+    ship_mglt = Column(Integer, nullable=False)
+    ship_consumables = Column(String(250), nullable=False)
+
+class Favorite(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    favorite_name = Column(String(250))
+    single_view_link = Column(String(250), ForeignKey('users.id'))
+
+
+##    what does this function do?
+##    def to_dict(self):
+##        return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
